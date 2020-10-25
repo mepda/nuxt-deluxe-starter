d54253bd2537474fae4c858e1d3dc09e9eef242d
ben sadick <bsadick@gmail.com>
2020-10-25 23:20:39 +0900

    removed hard coded posts into data

---
+++ components/DropDown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="countries">
+      Select Country:
+    </label>
+    <select @change="dropDown(e)">
+      <option value="US">US</option>
+      <option value="KR">KR</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    dropDown(e) {
+      console.log(e.target.value);
+    }
+  }
+};
+</script>
+
+<style></style>
